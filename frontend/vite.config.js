d8e14934b7a041cd3afad2b9b679448e1ba498cb
Travis028 <waynetavis123@gmail.com>
2026-06-24 07:51:08 +0300

    Disable PWA service worker caching to allow instant updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,63 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['app_icon-192.png', 'app_icon-512.png', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'APOLLO J. FIZVALENTINE OWINO Memorial',
-        short_name: 'Memorial',
-        description: 'In Loving Memory of APOLLO J. FIZVALENTINE OWINO',
-        theme_color: '#1A2530',
-        background_color: '#1A2530',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'app_icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'app_icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        // Cache all static files and images
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 },
-              cacheableResponse: { statuses: [0, 200] }
-            }
-          },
-          {
-            urlPattern: /\/api\/(grandpa|family|program|memories|life_photos|tributes)/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-data-cache',
-              expiration: { maxEntries: 50, maxAgeSeconds: 60 * 60 * 24 * 30 },
-              networkTimeoutSeconds: 5
-            }
-          },
-          {
-            urlPattern: /\/api\/static\/images\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'image-cache',
-              expiration: { maxEntries: 200, maxAgeSeconds: 60 * 60 * 24 * 30 },
-              cacheableResponse: { statuses: [0, 200] }
-            }
-          }
-        ]
-      }
-    })
+      // VitePWA removed to prevent aggressive caching of old versions
   ],
   base: '/',
   build: {
